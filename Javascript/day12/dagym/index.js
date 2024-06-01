4b21c2664ca4636da63fb01628c5893f178d9f1b
Rohyu-jeong <95173047+Rohyu-jeong@users.noreply.github.com>
2024-06-02 00:37:00 +0900

    Add files via upload

--- Javascript/day12/dagym/index.js
+++ Javascript/day12/dagym/index.js
@@ -1,141 +1,226 @@
-import { centerList } from "./data.js";
+import { centerList } from "./centerData.js";
+import { liteAdList } from "./liteAdData.js";
 
-const contentList = document.querySelector(".contentList");
+class daGym {
 
-const makeTags = (tags) => {
-    const tag = tags.join(" / ")
-    return `<div class="infoTags">${tag}</div>`
-}
+    constructor(list) {
+        this.gymName = list.gymName;
+        this.gymPhotoSmall = list.gymPhotoSmall;
+        this.address = list.address;
+        this.isWomenOnly = list.isWomenOnly;
+        this.isReservationRequired = list.isReservationRequired;
+        this.hasDailyItem = list.hasDailyItem;
+        this.isLowestPrice = list.isLowestPrice;
+        this.tags = list.tags;
+        this.program = list.program.free;
+        this.service = list.service.free;
+        this.count = list.price.count;
+        this.isLowestPrice = list.price.isLowestPrice;
+        this.lowestPrice = list.price.lowestPrice;
+        this.originalPrice = list.price.originalPrice;
+        this.period = list.price.period;
+        this.reviewRate = list.review.rate;
+        this.reviewCount = list.review.count;
+    }
 
-const makeGymName = (gymName) => `<div class="infoGymName">${gymName}</div>`
+    makeTags () {
+        const tag = this.tags.join(" / ")
+        return `<div class="infoTags">${tag}</div>`
+    }
 
-const makeReview = (reviewRate, reviewCount) => {
-    const review = `
+    makeGymName () {
+        return `<div class="infoGymName">${this.gymName}</div>`
+    }
+
+    makeReview () {
+        return `
         <div class="review">
             <span class="reviewStar">
                 <i class="fa-solid fa-star"></i>
-                ${reviewRate}
+                ${this.reviewRate}
             </span>
             <span class="reviewCount">
-                (${reviewCount})
+                (${this.reviewCount})
             </span>
         </div>
-    `
-    return reviewRate != 0 ? review : ""
-}
+        `
+    }
 
-const makeAddress = (address) => `<span class="infoAddress">${address}</span>`
+    makeAddress () {
+        return `<span class="infoAddress">${this.address}</span>`
+    }
 
-const makeReservation = (isReservationRequired) => {
-    const reservation = `
+    makeReservation () {
+        const reservation = `
         <div class="infoReser">
-            <i class="fa-solid fa-calendar-days"></i>
+            <div class="infoReserIcon">
+                <i class="fa-solid fa-calendar-days"></i>
+            </div>
             <span>예약필요</span>
         </div>
-    `
-    return isReservationRequired ? reservation : ""
-}
+        `
+        return this.isReservationRequired ? reservation : ""
+    }
 
-const makeDailyItem= (hasDailyItem) => {
-    const daily = `
+    makeDailyItem () {
+        const daily = `
         <span class="dailyLowest">일일권</span>
-    `
-    return hasDailyItem ? daily : ""
-}
+        `
+    return this.hasDailyItem ? daily : ""
+    }
 
-const makeLowestPrice= (isLowestPrice) => {
-    const lowest = `
+    makeLowestPrice () {
+        const lowest = `
         <span class="dailyLowest">다짐회원가</span>
-    `
-    return isLowestPrice ? lowest : ""
-}
+        `
+        return this.isLowestPrice ? lowest : ""
+    }
 
-const makePrice = (originalPrice, lowestPrice, period, count) => {
-    const periodOrCount = period == null ? count : period
-    const originalP = parseInt(Math.floor(originalPrice / periodOrCount / 10) * 10)
-    const lowestP = parseInt(Math.floor(lowestPrice / periodOrCount / 10) * 10)
-    const discount = parseInt((originalPrice - lowestPrice) / originalPrice * 100)
+    makePrice () {
+        const periodOrCount = this.period == null ? this.count : this.period
+        const originalP = parseInt(Math.floor(this.originalPrice / periodOrCount / 10) * 10)
+        const lowestP = parseInt(Math.floor(this.lowestPrice / periodOrCount / 10) * 10)
+        const discount = parseInt((this.originalPrice - this.lowestPrice) / this.originalPrice * 100)
 
-    const lowest = `
-    <div class="priceL">
-        <span class="priceN">${lowestP}원~</span>
-        <span class="priceM">/월</span>
-    </div>
-    `
-    const origin = `
-    <div class="priceO">
-        <div class="original">${originalP}원</div>
-        <div class="disPrice">
-        <span class="discount">${discount}%</span>
-        <span class="priceN">${lowestP}원~</span>
-        <span class="priceM">/월</span>
+        const lowest = `
+        <div class="priceL">
+            <span class="priceN">${lowestP}원~</span>
+            <span class="priceM">/월</span>
         </div>
-    </div>
-    `
+        `
+        const origin = `
+        <div class="priceO">
+            <div class="original">${originalP}원</div>
+            <div class="disPrice">
+            <span class="discount">${discount}%</span>
+            <span class="priceN">${lowestP}원~</span>
+            <span class="priceM">/월</span>
+            </div>
+        </div>
+        `
 
-    return originalPrice == lowestPrice ? lowest : origin
-}
+        return this.isLowestPrice ? origin : lowest
+    }
 
-const makeProgramFree = (program) => {
-    const programList = `        
+    makeProgramFree () {
+        const programList = `        
         <div class="programBoxFree">
             <div class="programFreeName">무료 프로그램</div>
-            <span class="programFreeNameList">${program}</span>
+            <span class="programFreeNameList">${this.program}</span>
         </div>
         `
-    return program == "" ? "" : programList;
-}
+        return this.program == "" ? "" : programList;
+    }
 
-const makeProgramService = (service) => {
-    const serviceList = `        
+    makeProgramService () {
+        const serviceList = `        
         <div class="programBoxService">
             <div class="programFreeName">무료 서비스</div>
-            <span class="programFreeNameList">${service}</span>
+            <span class="programFreeNameList">${this.service}</span>
         </div>
         `
-    return service == "" ? "" : serviceList;
-}
+        return this.service == "" ? "" : serviceList;
+    }
 
+    makeInfos () {
+       return `
+        <div class="infos">
+            ${this.makeTags ()}
+            ${this.makeGymName ()}
+            <div class="reviewAddress">
+                ${this.makeReview()}
+                ${this.makeAddress()}
+            </div>
+            ${this.makeReservation()}
+        </div>
+        <div class="infoDailyPrice">
+            <div class="infoDailyLosest">
+                ${this.makeDailyItem()}
+                ${this.makeLowestPrice()}
+            </div>
+            ${this.makePrice()}
+        </div>
+        `
+    }
 
-centerList.forEach((v) => {
-  contentList.insertAdjacentHTML(
-    "beforeend",
-    `
-        <div class="content">
+    makeLiteAdList (first) {
+        first.insertAdjacentHTML("beforeend", 
+        `
+        <div class="firstContent">
         <div class="contentBox">
-        <div class="imgBox">
+        <div class="fistImgBox">
             <img
-            src="${v.gymPhotoSmall}"
+            src="${this.gymPhotoSmall}"
             alt=""
             />
         </div>
-        <div class="info">
-            <div class="infoHeart">
+        <div class="fistInfo">
+            <div class="firstHeart">
             <i class="fa-regular fa-heart"></i>
             </div>
-            <div class="infos">
-            ${makeTags(v.tags)}
-            ${makeGymName(v.gymName)}
-                <div class="reviewAddress">
-                    ${makeReview(v.review.rate, v.review.count)}
-                    ${makeAddress(v.address)}
-                </div>
-            ${makeReservation(v.isReservationRequired)}
-            </div>
-            <div class="infoDailyPrice">
-            <div class="infoDailyLosest">
-            ${makeDailyItem(v.hasDailyItem)}
-            ${makeLowestPrice(v.isLowestPrice)}
-            </div>
-            ${makePrice(v.price.originalPrice, v.price.lowestPrice, v.price.period, v.price.count)}
-            </div>
+            ${this.makeInfos()}
         </div>
         </div>
-        <div class="programBox">
-        ${makeProgramFree(v.program.free)}
-        ${makeProgramService(v.service.free)}
+        <div class="firstProgramBox">
+        ${this.makeProgramFree()}
+        ${this.makeProgramService()}
         </div>
     </div>
     `
-  );
-});
+        );
+    }
+
+    makeCenterList (second) {
+        second.insertAdjacentHTML(
+            "beforeend",
+            `
+                <div class="secondContent">
+                <div class="contentBox">
+                <div class="secondImgBox">
+                    <img
+                    src="${this.gymPhotoSmall}"
+                    alt=""
+                    />
+                </div>
+                <div class="secondInfo">
+                    <div class="secondHeart">
+                    <i class="fa-regular fa-heart"></i>
+                    </div>
+                    ${this.makeInfos()}
+                </div>
+                </div>
+                <div class="secondProgramBox">
+                ${this.makeProgramFree()}
+                ${this.makeProgramService()}
+                </div>
+            </div>
+            `
+          );
+    }
+
+}
+
+const first = document.querySelector(".first");
+const second = document.querySelector(".second");
+
+const glass = document.querySelector(".glass");
+
+const dailyBtn = document.querySelector(".dailyBtn");
+const lowestBtn = document.querySelector(".lowestBtn");
+
+const tagList = ["헬스", "P.T", "기구필라테스", "G.X", "골프", "필라테스", "요가", "사우나", "크로스핏", "주짓수", "복싱", "그룹P.T", "점핑운동"];
+
+liteAdList.forEach((v) => {
+    const liteAd = new daGym(v);
+    liteAd.makeLiteAdList(first);
+})
+
+centerList.forEach((v) => {
+    const center = new daGym(v);
+    center.makeCenterList(second);
+})
+
+liteAdList.forEach ((v) => {
+
+})
+
---
+++ Javascript/day12/dagym/liteAdData.js
@@ -0,0 +1,93 @@
+export const liteAdList = [
+    {
+        "_id": "65fb96e46d09b7001d9d1fa0",
+        "gymName": "시청 무브짐앤무브맥스PT",
+        "gymPhotoBig": "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/18e5ec56c2a2c8a2e4/2WMqrmKY6Sakmdrwo7uXrELngTjjummNA7rG.Big_240324_무브짐앤무브맥스PT시청점_13.jpg",
+        "gymPhotoSmall": "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/18e5ec56c2a2c8a2e4/2YUds6u8BgAsm4SVkFLcbpLqT6Y1au448n6K.Small_240324_무브짐앤무브맥스PT시청점_13.jpg",
+        "address": "서울 중구 서소문동",
+        "location": [
+            126.97363348811612,
+            37.563331828432155
+        ],
+        "isWomenOnly": false,
+        "isReservationRequired": false,
+        "hasDailyItem": true,
+        "isLiked": null,
+        "isLowestPrice": true,
+        "exhibitionRemainingCount": null,
+        "tags": [
+            "헬스"
+        ],
+        "program": {
+            "free": ""
+        },
+        "service": {
+            "free": "샤워실 / 수건 / 대형제빙기 / ️ 무료주차 2시간 30분 / 인바디 측정"
+        },
+        "price": {
+            "name": "헬스 회원권",
+            "count": null,
+            "isLowestPrice": true,
+            "lowestPrice": 450000,
+            "originalPrice": 528000,
+            "period": 12,
+            "priceType": "period",
+            "itemTags": [
+                "헬스"
+            ],
+            "lowestPriceRemainingCount": 10
+        },
+        "review": {
+            "rate": 5,
+            "count": 4
+        },
+        "videoUrl": null,
+        "videoThumbnailUrl": null
+    },
+    {
+        "_id": "59e4360761a74379ba5738ac",
+        "gymName": "서대문역 VIP 휘트니스",
+        "gymPhotoBig": "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/15f23730f0d4746062/4K6CpJYkn2FxX8CjH1MrrvAGtZsr251Gk9JabUig3AHm.jpg",
+        "gymPhotoSmall": "https://s3.ap-northeast-2.amazonaws.com/stone-i-dagym-centers/images/gyms/15f23730f0d4746062/4K6CpJYkn2FxX8CjH1MrrvAGtZsr251Gk9JabUig3AHn.jpg",
+        "address": "서울 서대문구 충정로2가",
+        "location": [
+            126.964323,
+            37.564755
+        ],
+        "isWomenOnly": false,
+        "isReservationRequired": false,
+        "hasDailyItem": false,
+        "isLiked": null,
+        "isLowestPrice": true,
+        "exhibitionRemainingCount": null,
+        "tags": [
+            "헬스",
+            "P.T"
+        ],
+        "program": {
+            "free": "‍ O.T (3개월 이상 등록시 - 30분 1회 )"
+        },
+        "service": {
+            "free": "운동복 / 수건 / 샤워실 / 인바디 / 주차는 리프트안 승용차만 2시간가능"
+        },
+        "price": {
+            "name": "헬스+운동복",
+            "count": null,
+            "isLowestPrice": false,
+            "lowestPrice": 462000,
+            "originalPrice": 462000,
+            "period": 12,
+            "priceType": "period",
+            "itemTags": [
+                "헬스"
+            ],
+            "lowestPriceRemainingCount": null
+        },
+        "review": {
+            "rate": 4.94,
+            "count": 13
+        },
+        "videoUrl": null,
+        "videoThumbnailUrl": null
+    }
+]
