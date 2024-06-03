180f8b2c563305431d9add5452928c30437a0fb9
Rohyu-jeong <95173047+Rohyu-jeong@users.noreply.github.com>
2024-06-03 23:49:03 +0900

    Add files via upload

--- Javascript/day15/js/date.js
+++ Javascript/day15/js/date.js
@@ -1,5 +1,4 @@
-export const makeDate = () => {
-    const dateInfo = new Date();
+export const makeDate = (date) => {
     const dateInfos = {
         month : {
             1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12:"December"
@@ -12,11 +11,11 @@ export const makeDate = () => {
         }
     }
 
-    const makeDay = (dateInfo) => dateInfo < 3 ? dateInfo + dateInfos.day[dateInfo] : dateInfo + dateInfos.day[4]
+    const makeDay = (date) => date < 3 ? date + dateInfos.day[date] : date + dateInfos.day[4]
 
     return `
-        <span class="week">${dateInfos.week[dateInfo.getDay()]}, </span>
-        <span class="monthDay">${dateInfos.month[dateInfo.getMonth() + 1]} ${makeDay(dateInfo.getDate())}</span>
+        <span class="week">${dateInfos.week[date.getDay()]}, </span>
+        <span class="monthDay">${dateInfos.month[date.getMonth() + 1]} ${makeDay(date.getDate())}</span>
     `
  }
 
--- Javascript/day15/js/index.js
+++ Javascript/day15/js/index.js
@@ -1,39 +1,41 @@
 import { makeDate } from "./date.js";
+import { dataList } from "./tododata.js";
+import { addToDoList } from "./addtodolist.js"
+import { addNewEvent } from "./addtodo.js"
+import { makeTasks } from "./task.js";
+import { TaskCount } from "./task.js";
+
+const dateInfo = new Date();
 
 
 const date = document.querySelector(".date");
-date.innerHTML = makeDate();
-
-
-
-
-let newToDo;
-let newTime;
-
-const addNew = document.querySelector(".addNew");
 const toDoList = document.querySelector(".toDoList");
+const prevBtn = document.querySelector(".prevBtn"); // 이전 날짜
+const nextBtn = document.querySelector(".nextBtn"); // 다음 날짜
 
-addNew.addEventListener("click", ()=> {
-    newToDo = window.prompt("New To Do");
-    newTime = window.prompt("New Time");
+date.innerHTML = makeDate(dateInfo); // 오늘 날짜
 
-    newTime = newTime == null ? "ALL DAY" : newTime;
+let currentDay = dateInfo.getDate() -1; // 화면에 보이는 day
+let dayList = dataList[currentDay].dataListToDoLists; // currentDay의 todo 배열
 
-    toDoList.innerHTML = `
-        <div class="toDoContent">
-            <div class="checkList">
-                <div class="checkIcon">
-                    <i class="fa-regular fa-square"></i>
-                    <i class="fa-solid fa-square-check hidden"></i>
-                </div>
-                <div class="toDo">
-                    <span>${newToDo}</span>
-                </div>
-            </div>
-            <div class="time">
-                <span>${newTime}</span>
-            </div>
-        </div>
-        `
+addToDoList(dayList, toDoList); // 기존 리스트 불러오기
+
+let toDoContents = document.querySelectorAll(".toDoContent");
+
+let count = TaskCount(toDoContents);
+
+makeTasks(count);
+
+addNewEvent(dataList, currentDay, toDoList); // 추가 버튼
+
+prevBtn.addEventListener("click", () => {
+    currentDay--;
+
+    currentDay = currentDay == 0 ? currentDay = 30 : currentDay;
 })
+
+
+
+
+
 
